7ff87bb92f6446fd6513ca190687ea30134ef4fc
dipesh-ops <124789358+dipesh-ops@users.noreply.github.com>
2025-10-16 10:14:40 +0530

    Fixed buttons size in category page

--- src/Components/CategoryPage.jsx
+++ src/Components/CategoryPage.jsx
@@ -78,10 +78,10 @@ const CategoryPage = () => {
                                 </p>
 
                                 {cartItems.some((p)=> p.id === item.id) ? 
-                                    <button onClick={()=> deleteCart(item)} className='bg-orange-500 px-6 py-3 w-full sm:w-1/2 lg:w-1/3 rounded-lg text-white hover:bg-orange-600 cursor-pointer'>
+                                <button onClick={()=> deleteCart(item)} className='bg-orange-500 px-6 py-3 w-full rounded-lg text-white hover:bg-orange-600 cursor-pointer'>
                                          Remove From Cart
                                     </button> : 
-                                    <button onClick={()=> addToCartFunction(item)} className='bg-emerald-500 px-6 py-3 w-full sm:w-1/2 lg:w-1/3 rounded-lg text-white hover:bg-emerald-600 cursor-pointer'>
+                                    <button onClick={()=> addToCartFunction(item)} className='bg-emerald-500 px-6 py-3 w-full rounded-lg text-white hover:bg-emerald-600 cursor-pointer'>
                                           Add to Cart
                                     </button> 
                   }
